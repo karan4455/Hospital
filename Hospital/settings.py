8e9466a932a11702dfddb015a67ead04ac1459b4
Karan Jethava <97521514+karan4455@users.noreply.github.com>
2023-02-20 21:50:56 +0530

    update

--- Hospital/settings.py
+++ Hospital/settings.py
@@ -130,7 +130,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # EMAIL_USE_TLS = True
